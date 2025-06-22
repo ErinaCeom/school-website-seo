6abb82ee683105fe7bfeccb693aee01f8a918dfd
ErinaCeom <166990379+ErinaCeom@users.noreply.github.com>
2025-06-22 13:06:52 +0000

    footer responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 import { Stack, Box, Grid, Typography, Link } from "@mui/material";
 import Image from "next/image";
 
-const links : string[] = [
+const links: string[] = [
   "http://www.educationboardresults.gov.bd/",
   "https://moedu.portal.gov.bd/",
   "https://dinajpureducationboard.gov.bd/",
@@ -20,8 +20,11 @@ export default function Footer() {
         <Stack direction="row" justifyContent="space-around">
           {/* Quick Links */}
           <Grid container>
-            <Grid size={{ xs: "auto", sm: 6, md: 12 }}>
-              <Typography variant="h5" sx={{ fontFamily: "Forum", mb: 2 }}>
+            <Grid size={{ xs: "auto", md: 12 }}>
+              <Typography
+                variant="h5"
+                sx={{ fontFamily: "Forum", fontSize: { md: "3rem" }, mb: 2 }}
+              >
                 Quick links
               </Typography>
               <Grid container spacing={2}>
@@ -33,7 +36,10 @@ export default function Footer() {
                         href={text}
                         underline="always"
                         color="inherit"
-                        sx={{ fontSize: "small", fontFamily: "Josefin Sans" }}
+                        sx={{
+                          fontSize: { md: "1.2rem" },
+                          fontFamily: "Josefin Sans",
+                        }}
                       >
                         {text}
                       </Link>
@@ -49,7 +55,10 @@ export default function Footer() {
                           href={text}
                           underline="always"
                           color="inherit"
-                          sx={{ fontSize: "small", fontFamily: "Josefin Sans" }}
+                          sx={{
+                            fontSize: { md: "1.2rem" },
+                            fontFamily: "Josefin Sans",
+                          }}
                         >
                           {text}
                         </Link>
@@ -62,8 +71,11 @@ export default function Footer() {
           </Grid>
           {/*Useful links*/}
           <Grid container>
-            <Grid size={{ xs: "auto", sm: 6, md: 12 }}>
-              <Typography variant="h5" sx={{ fontFamily: "Forum", mb: 2 }}>
+            <Grid size={{ xs: "auto", md: 12 }}>
+              <Typography
+                variant="h5"
+                sx={{ fontFamily: "Forum", fontSize: { md: "3rem" }, mb: 2 }}
+              >
                 Useful links
               </Typography>
               <Stack spacing={0}>
@@ -72,14 +84,17 @@ export default function Footer() {
                   "Ministry of education",
                   "Dinajpur education board",
                   "Banbeis",
-                ].map((text,index) => (
+                ].map((text, index) => (
                   <Link
                     key={text}
                     href={links[index]}
                     target="_blank"
                     underline="always"
                     color="inherit"
-                    sx={{ fontSize: "small", fontFamily: "Josefin Sans" }}
+                    sx={{
+                      fontSize: { md: "1.2rem" },
+                      fontFamily: "Josefin Sans",
+                    }}
                   >
                     {text}
                   </Link>
@@ -89,17 +104,50 @@ export default function Footer() {
           </Grid>
         </Stack>
       </Box>
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1, alignSelf: "center" }}>
         <Stack
           direction="column"
           justifyContent="center"
           alignItems="center"
           textAlign="center"
-          pb={3}
         >
-          <Image src="/logo.png" alt="Logo" width={50} height={50} />
-          <Typography>
-            Copyright © 2023 <br /> St. Philip&apos;s High School &amp; College
+          <Box
+            sx={{
+              position: "relative",
+              width: {
+                xs: 80,
+                md: 100,
+              },
+              height: {
+                xs: 80,
+                md: 100,
+              },
+            }}
+          >
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              fill
+              style={{ objectFit: "contain" }}
+            />
+          </Box>
+
+          <Typography
+            sx={{
+              fontSize: {
+                md: "1.5rem",
+              },
+              mt: 1,
+            }}
+          >
+            St. Philip&apos;s High School &amp; College
+          </Typography>
+          <Typography
+            sx={{
+              color: "grey",
+            }}
+          >
+          Copyright © 2023 All rights reserved
           </Typography>
         </Stack>
       </Box>
