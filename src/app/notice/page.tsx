afce55be5b6038a4df6e2f40fbdd4140e865e030
ErinaCeom <166990379+ErinaCeom@users.noreply.github.com>
2025-07-01 11:11:41 +0000

    Added functional category filtering and other buttons

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import {
   Container,
@@ -16,22 +15,22 @@ import FilterListIcon from "@mui/icons-material/FilterList";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CategoryFilter from "@/components/CategoryFilter";
 
-export default function Notice() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+import { getNotice } from "@/utils";
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+export default async function Notice({
+  searchParams,
+}: {
+  searchParams: { category?: string };
+}) {
+  const category = searchParams.category;
+  const notices = await getNotice(category);
 
   return (
     <Container
       maxWidth="xl"
-      sx={{ height: { xs: "max-content", md: "100vh" }, py: 10, border: 1 }}
+      sx={{ height: { xs: "max-content", md: "100vh" }, py: 10, border: 0 }}
     >
       <Typography sx={{ fontSize: "2rem", fontWeight: "bold" }}>
         All announcements
@@ -41,42 +40,8 @@ export default function Notice() {
       </Typography>
 
       <Box sx={{ backgroundColor: "black", my: 2 }}>
-        <Stack direction="row" spacing={2}>
-          {/* High School Dropdown Button */}
-          <Button
-            variant="outlined"
-            endIcon={<ExpandMoreIcon />}
-            onClick={handleClick}
-            sx={{
-              color: "white",
-              borderColor: "white",
-              borderRadius: "12px",
-              textTransform: "none",
-              fontFamily: "Josefin Sans",
-              fontSize: "16px",
-              px: 2,
-            }}
-          >
-            High school
-          </Button>
-          <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            PaperProps={{
-              sx: {
-                backgroundColor: "#2a2a2a", // dark gray background
-                color: "white", // white text
-                borderRadius: "12px",
-                minWidth: 160,
-              },
-            }}
-          >
-            <MenuItem onClick={handleClose}>Pre-Primary</MenuItem>
-            <MenuItem onClick={handleClose}>Primary</MenuItem>
-            <MenuItem onClick={handleClose}>High school</MenuItem>
-            <MenuItem onClick={handleClose}>College</MenuItem>
-          </Menu>
+        <Stack direction="row" gap={2}>
+          <CategoryFilter />
           {/* Icon Buttons */}
           {[<FilterListIcon />, <SearchIcon />, <NotificationsNoneIcon />].map(
             (icon, i) => (
@@ -95,51 +60,54 @@ export default function Notice() {
           )}
         </Stack>
       </Box>
-
-      <Stack spacing={2}>
-        <Stack>
-          <Stack
-            direction="row"
-            spacing={1}
-            sx={{ justifyContent: "flex-start", alignItems: "flex-start" }}
-          >
-            <Typography color="grey">
-              {new Date().toLocaleDateString("en-GB", {
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-              })}
-            </Typography>
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{
-                border: 3,
-                borderRadius: 8,
-                borderColor: "gray",
-                height: "1px",
-                alignSelf: "center",
-              }}
-            />
-            <Typography color="grey">High school </Typography>
-          </Stack>
-          <Link
-            href="#"
-            underline="always"
-            color="inherit"
-            sx={{
-              fontSize: { md: "1.2rem" },
-              fontFamily: "Josefin Sans",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              display: "-webkit-box",
-              WebkitLineClamp: 2,
-              WebkitBoxOrient: "vertical",
-            }}
-          >
-            Pre-Primary class routine for extreme heat wave
-          </Link>
-        </Stack>
+      <Stack spacing={3}>
+        {notices.map((notice, i) => {
+          return (
+            <Stack key={i}>
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{ justifyContent: "flex-start", alignItems: "flex-start" }}
+              >
+                <Typography color="grey">
+                  {new Date(notice.date).toLocaleDateString("en-GB", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </Typography>
+                <Divider
+                  orientation="vertical"
+                  flexItem
+                  sx={{
+                    border: 3,
+                    borderRadius: 8,
+                    borderColor: "gray",
+                    height: "1px",
+                    alignSelf: "center",
+                  }}
+                />
+                <Typography color="grey">{notice.category}</Typography>
+              </Stack>
+              <Link
+                href="#"
+                underline="always"
+                color="inherit"
+                sx={{
+                  fontSize: { md: "1.2rem" },
+                  fontFamily: "Josefin Sans",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitLineClamp: 2,
+                  WebkitBoxOrient: "vertical",
+                }}
+              >
+                {notice.title}
+              </Link>
+            </Stack>
+          );
+        })}
       </Stack>
     </Container>
   );
