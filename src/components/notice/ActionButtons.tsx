b5f7c33c99c052eae458abd396ecc46b5e61288e
saz <92567869+MrTeraByte@users.noreply.github.com>
2025-07-15 10:05:00 +0000

    Disabled searchbar

--- src/components/notice/ActionButtons.tsx
+++ src/components/notice/ActionButtons.tsx
@@ -3,21 +3,21 @@
 import React from "react";
 import { Stack, IconButton } from "@mui/material";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import { useSearchParams } from "next/navigation";
+//import { useSearchParams } from "next/navigation";
 
 import CategoryFilter from "./actionButtons/CategoryFilter";
 import SortFilterMenu from "./actionButtons/SortFilter";
 import SearchBar from "./actionButtons/SearchBar";
 
-export default function ClientNoticeControls() {
-  const searchParams = useSearchParams();
-  const hasQuery = !!searchParams.get("q");
+export default function ActionButtons() {
+  //const searchParams = useSearchParams();
+  //const hasQuery = !!searchParams.get("q");
 
   // this state fully drives hiding/showing
-  const [searchOpen, setSearchOpen] = React.useState(hasQuery);
-  React.useEffect(() => {
-    setSearchOpen(!!searchParams.get("q"));
-  }, [searchParams]);
+  const [searchOpen, setSearchOpen] = React.useState(false);
+  /*React.useEffect(() => {
+    //setSearchOpen(!!searchParams.get("q"));
+  }, [searchParams]);*/
 
   return (
     <>
